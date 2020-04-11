2138ded3caca2f2fa1758e683741149cdd6f6bf3
Thomas Applewhite <thomashapplewhite@gmail.com>
2020-04-11 15:38:24 -0700

    Halfway Point
    About to start  collision handling, though I'd save here.

---
+++ src/scenes/Play.js
@@ -0,0 +1,64 @@
+class Play extends Phaser.Scene {
+    constructor() {
+        super("playScene");
+    }
+
+    preload(){
+        //load images/title sprites
+        this.load.image('rocket', './assets/rocket.png');
+        this.load.image('spaceship', './assets/spaceship.png');
+        this.load.image('starfield', './assets/starfield.png');
+    }
+
+    create() {
+        //place tile sprite
+        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0, 0);
+        //white rectangle borders
+        this.add.rectangle(5, 5, 630, 32, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(5, 443, 630, 32, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(5, 5, 32, 455, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(603, 5, 32, 455, 0xFFFFFF).setOrigin(0, 0);
+        //green UI background
+        this.add.rectangle(37, 42, 566, 64, 0x00FF00).setOrigin(0,0);
+        //add rocket (p1)
+        this.p1Rocket = new Rocket(this, game.config.width/2, 431, 'rocket').setScale(0.5, 0.5).setOrigin(0, 0);
+        //add spaceships (x3)
+        this.ship01 = new Spaceship(this, game.config.width + 192, 132, 'spaceship', 0, 30).setOrigin(0, 0);
+        this.ship02 = new Spaceship(this, game.config.width + 96, 196, 'spaceship', 0, 20).setOrigin(0, 0);
+        this.ship03 = new Spaceship(this, game.config.width, 260, 'spaceship', 0, 10).setOrigin(0, 0);
+        //define keys
+        keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+    }
+
+    update(){
+        this.starfield.tilePositionX -= 4;
+        this.p1Rocket.update();
+        this.ship01.update();           //update spaceships (x3)
+        this.ship02.update();
+        this.ship03.update();
+        //check collisions
+        if(this.checkCollision(this.p1Rocket, this.ship03)) {
+            console.log('kaboom ship 03');
+        }
+        if(this.checkCollision(this.p1Rocket, this.ship02)) {
+            console.log('kaboom ship 02');
+        }
+        if(this.checkCollision(this.p1Rocket, this.ship01)) {
+            console.log('kaboom ship 01');
+        }
+    }
+
+    checkCollision(rocket, ship){
+        //simple AABB checking
+        if (rocket.x < ship.x + ship.width &&
+            rocket.x  + rocket.width > ship.x &&
+            rocket.y < ship.y + ship.height &&
+            rocket.height + rocket.y > ship.y){
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
