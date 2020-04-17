64f8666eb4072029e0da61e1276d0a6fcd3cd820
Thomas Applewhite <thomashapplewhite@gmail.com>
2020-04-17 16:22:31 -0700

    final header comment

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
 /*
-header comment goes here, eventually
+Thomas Applewhite
+All work is my own or provided from the tutorial
+---Point Breakdown---
+S-Tier:
+    simultaneous two-player mode: 50 points
+Intermediate Tier:
+    new timing/scoring mechanism that adds time to the clock for successful hits: 25 points
+Novice Tier:
+    display the time remaining (in seconds) on the screen: 15 points
+Starting Tier:
+    speed increase that happens after 30 seconds in the original game: 10 points
+    allow the player to control the Rocket after it's fired: 10 points
+Final Score: 110/100
+I know I can't go over 100 but I forgot the timer was on the list so I went over by accident
 */
 
 
